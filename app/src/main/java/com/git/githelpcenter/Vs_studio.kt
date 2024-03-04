f80025c3bf82dffb9c945ccc0254321fbd552d89
harita <haritaginoya@gmail.com>
2024-03-04 18:45:12 +0530

    first

---
+++ app/src/main/java/com/git/githelpcenter/Vs_studio.kt
@@ -0,0 +1,18 @@
+package com.git.githelpcenter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+
+class Vs_studio : AppCompatActivity() {
+    lateinit var recycle : RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vs_studio)
+
+
+
+
+    }
+}
